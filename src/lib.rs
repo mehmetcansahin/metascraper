131269860196c4d9d6d54e7bc93391fb059239a9
mehmetcansahin <sahinmcan@gmail.com>
2022-12-23 19:39:37 +0300

    update test and rename

--- src/lib.rs
+++ src/lib.rs
@@ -19,14 +19,14 @@ impl HtmlParser<'_> {
                     .get("name")
                     .or_else(|| tag.attributes().get("property"))
                     .flatten()
-                    .and_then(|x| Some(x.as_utf8_str().to_string()));
+                    .map(|x| x.as_utf8_str().to_string());
 
                 let content = tag
                     .attributes()
                     .get("content")
                     .or_else(|| tag.attributes().get("description"))
                     .flatten()
-                    .and_then(|x| Some(x.as_utf8_str().to_string()));
+                    .map(|x| x.as_utf8_str().to_string());
 
                 if name.is_some() && content.is_some() {
                     let nt = Metatag {
@@ -45,7 +45,7 @@ impl HtmlParser<'_> {
             .query_selector(selector)
             .and_then(|mut iter| iter.next())
             .and_then(|node_handle| node_handle.get(self.parser))
-            .and_then(|node| Some(node.inner_text(self.parser).to_string()))
+            .map(|node| node.inner_text(self.parser).to_string())
     }
 
     fn get_attribute(&self, selector: &str, attr: &str) -> Option<String> {
@@ -55,7 +55,7 @@ impl HtmlParser<'_> {
             .and_then(|node_handle| node_handle.get(self.parser))
             .and_then(|node| node.as_tag())
             .and_then(|html_tag| html_tag.attributes().get(attr).flatten())
-            .and_then(|bytes| Some(bytes.as_utf8_str().to_string()))
+            .map(|bytes| bytes.as_utf8_str().to_string())
     }
 
     fn rss(&self) -> Option<String> {
@@ -89,7 +89,7 @@ pub struct Metatag {
 }
 
 #[derive(Debug)]
-pub struct PageInfo {
+pub struct MetaScraper {
     pub title: Option<String>,
     pub description: Option<String>,
     pub canonical: Option<String>,
@@ -98,12 +98,12 @@ pub struct PageInfo {
     pub metatags: Option<Vec<Metatag>>,
 }
 
-impl PageInfo {
-    pub fn from_str(input: &str) -> PageInfo {
-        let dom = tl::parse(&input, tl::ParserOptions::default()).unwrap();
+impl MetaScraper {
+    pub fn parse(input: &str) -> MetaScraper {
+        let dom = tl::parse(input, tl::ParserOptions::default()).unwrap();
         let parser = dom.parser();
         let html_parser = HtmlParser::new(&dom, parser);
-        PageInfo {
+        MetaScraper {
             title: html_parser.title(),
             description: html_parser.description(),
             canonical: html_parser.canonical(),
@@ -111,43 +111,5 @@ impl PageInfo {
             rss: html_parser.rss(),
             metatags: html_parser.metatags(),
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    fn parse_from_test_str() -> PageInfo {
-        let html = include_str!("test.html");
-        PageInfo::from_str(html)
-    }
-
-    #[test]
-    fn test_page() {
-        let page_info = parse_from_test_str();
-        assert_eq!(page_info.title, Some("Pageinfo".to_string()));
-        assert_eq!(page_info.language, Some("en".to_string()));
-        assert_eq!(
-            page_info.description,
-            Some("Pageinfo description".to_string())
-        );
-        assert_eq!(
-            page_info.canonical,
-            Some("https://mehmetcan.sahin.dev".to_string())
-        );
-        assert_eq!(page_info.rss, Some("rss.xml".to_string()));
-    }
-
-    #[test]
-    fn test_from_website() {
-        let resp = reqwest::blocking::get("https://mehmetcan.sahin.dev/")
-            .unwrap()
-            .text()
-            .unwrap();
-        let page_info = PageInfo::from_str(&resp);
-        assert_eq!(page_info.title, Some("Mehmetcan Şahin".to_string()));
-        assert_eq!(page_info.language, Some("en".to_string()));
-        assert_eq!(page_info.description, Some("Personal Page".to_string()));
     }
 }
